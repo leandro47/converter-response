0ae97345be0f171e7bea438c7a974a3ff54396f6
leandro47 <leandrosilva47@live.com>
2022-02-08 16:25:39 -0300

    Altered name method test

--- tests/ConverterTest.php
+++ tests/ConverterTest.php
@@ -6,13 +6,13 @@ use PHPUnit\Framework\TestCase;
 
 class ConverterTest extends TestCase
 {
-    public function testDecodeXml()
+    public function testInstanceClassDecodeFromXml()
     {
         $xml = file_get_contents('https://economia.awesomeapi.com.br/xml/last/USD-BRL');
 
         $converter =  new Converter($xml);
         $object = $converter->from(new Xml());
 
-        self::assertInstanceOf(Converter::class, $object);
+        TestCase::assertInstanceOf(Converter::class, $object);
     }
 }
